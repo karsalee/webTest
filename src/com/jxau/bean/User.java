47215e083ba11acef67f3ab68e231d159056ea2b
karsa_lee <krasa_lee@aliyun.com>
2019-05-10 09:58:54 +0800

    lls two commit

---
+++ src/com/jxau/bean/User.java
@@ -0,0 +1,67 @@
+package com.jxau.bean;
+
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+    private String userClass;
+    private String userSex;
+    private String userId;
+    private String userCity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserClass() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
+        this.userClass = userClass;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserCity() {
+        return userCity;
+    }
+
+    public void setUserCity(String userCity) {
+        this.userCity = userCity;
+    }
+}
